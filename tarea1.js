036579b6e250c0151223f6d112b611c518d6ff33
00095417 <00095417@uca.edu.sv>
2018-09-28 05:37:14 -0600

    primer commit

---
+++ tarea1.js
@@ -0,0 +1,10 @@
+class Producto{
+    constructor(codigo,descripPro,typeProduc,precCompra, precVenta,stock){
+        this.codigo = codigo;
+        this.descripPro = descripPro;
+        this.typeProduc = typeProduc;
+        this.precCompra = precCompra;
+        this.precVenta = precVenta;
+        this.stock = stock;
+    }
+}
